bc7be30e35f5bff2af632410f4b8b1ba210d3e64
CSE21098 <cse21098@gmail.com>
2026-02-13 08:54:31 +0000

    jjj.....

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,9 @@
 //const core = require("@actions/core");
 //const github =  require('@actions/github');
 //const exec =  require('@actions/exec');
-import core from "@actions/core";
-import github from "@actions/github";
-import exec from "@actions/exec";
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as exec from "@actions/exec";
 function run(){
 	core.notice('Hello World using JS!!!');
 }
