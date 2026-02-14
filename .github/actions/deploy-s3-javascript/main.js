176d2446e6510ab8bc7b38c04fa4063dfcbf7169
CSE21098 <cse21098@gmail.com>
2026-02-14 12:13:11 +0000

    Added URL to output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -10,6 +10,8 @@ function run(){
 	const s3Uri = `s3://${bucket}`
 	exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
+	const websiteUrl = `http://${bucket}.s3-website-${bucket-region}.amazonaws.com`;
+	core.setOuput('website-url',websiteURl);
 	core.notice('Hello World using JS!!!');
 }
 
