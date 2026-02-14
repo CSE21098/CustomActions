647fc847c1229cbace278984d87773f8c6edcd41
CSE21098 <cse21098@gmail.com>
2026-02-14 11:58:04 +0000

    Integrated S3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,10 +1,15 @@
-//const core = require("@actions/core");
-//const github =  require('@actions/github');
-//const exec =  require('@actions/exec');
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 import * as exec from "@actions/exec";
+
 function run(){
+	const bucket = core.getInput('bucket',{ required: true});
+	const bucketRegion = core.getInput('bucket-region',{ required: true});
+	const distFolder = core.getInput('dist-folder',{ required: true});
+
+	const s3Uri = `s3://${bucket}`
+	exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
 	core.notice('Hello World using JS!!!');
 }
 
